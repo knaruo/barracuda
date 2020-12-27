85ac7488b5a96f5f11d3131aa046ec059daa0fc8
knaruo <knaruo@gmail.com>
2020-12-27 21:15:47 +0900

    Resolved conflict of mounting holes and some diodes

--- pcb/barracuda/scripts/set_diode_positions.py
+++ pcb/barracuda/scripts/set_diode_positions.py
@@ -18,6 +18,17 @@ def set_diode_position_by_sw(pcb, sw_name):
     d_obj.SetPosition(pcbnew.wxPointMM(sw_x + X_OFS, sw_y + Y_OFS))
 
 
+def avoid_mounting_holes(pcb):
+    """Just a workaround to avoid mounting holes for some diodes."""
+    conflicted_diodes = [2, 3, 8, 9, 14, 15, 23, 24, 29, 30, ]
+    X_OFS_4_HOLE = - float(2.5)
+    for c_d in conflicted_diodes:
+        d_name = "D{}".format(c_d)
+        d = pcb.FindModuleByReference(d_name)
+        x, y = pcbnew.ToMM(d.GetPosition())
+        d.SetPosition(pcbnew.wxPointMM(x + X_OFS_4_HOLE, y))
+
+
 # Open the file and load the board
 file_name = "barracuda.kicad_pcb"
 file_path = os.path.join(os.getcwd(), "../{}".format(file_name))
@@ -29,6 +40,7 @@ switch_ids = range(1, 34)
 for sw_id in switch_ids:
     name = "SW" + str(sw_id)
     set_diode_position_by_sw(pcb, name)
+avoid_mounting_holes(pcb)
 
 # Save the board
 pcb.Save(file_path)
